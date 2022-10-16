d0af53270b964ef325baf0aa4771f3da6513fa7d
Daniel Grek Sanchez Castellanos <daniel.sanchez@kevin.eu>
2022-10-16 10:20:05 +0300

    buffer options

--- log-buffer.ts
+++ log-buffer.ts
@@ -3,20 +3,101 @@ import { prettyFactory } from "pino-pretty";
 // Pino-Http attaches request context to the log object.
 interface LogObject {
   reqId: string;
-  res: {
+  level: number;
+  res?: {
     statusCode: number;
   };
-  req: {
+  req?: {
     id: string;
   };
 }
 
+const logLevelToValueMap = {
+  fatal: 60,
+  error: 50,
+  warn: 50,
+  info: 30,
+  debug: 20,
+  trace: 10,
+};
+
+type LogLevelName = keyof typeof logLevelToValueMap;
+
+interface NumberRange {
+  from: number;
+  to: number;
+}
+
+export type LogBufferOptions = {
+  logLevelOnlyOnStatus: {
+    [key in LogLevelName]: {
+      statusCode: number | NumberRange;
+    };
+  };
+};
+
 export class LogBuffer {
-  logStorage: { [key: string]: object[] } = {};
+  logStorage: { [key: string]: { [level: number]: LogObject[] } } = {};
+
+  options: LogBufferOptions;
+
+  constructor(options: LogBufferOptions) {
+    this.options = options;
+  }
 
   private prettyPrinter = prettyFactory({
     colorize: true,
   });
+
+  private shouldLogBufferedLogs(statusCode: number): LogLevelName[] {
+    let toReleaseLevels: LogLevelName[] = [];
+
+    Object.entries(this.options.logLevelOnlyOnStatus).forEach(
+      ([levelName, levelOptions]) => {
+        const { statusCode: levelStatusCode } = levelOptions;
+
+        if (typeof levelStatusCode === "number") {
+          if (levelStatusCode === statusCode) {
+            toReleaseLevels.push(levelName as LogLevelName);
+          }
+        } else if (
+          statusCode >= levelStatusCode.from &&
+          statusCode <= levelStatusCode.to
+        ) {
+          toReleaseLevels.push(levelName as LogLevelName);
+        }
+      }
+    );
+
+    return toReleaseLevels;
+  }
+
+  private addLogToBuffer(logObject: LogObject) {
+    const reqId = logObject.reqId || logObject.req?.id;
+
+    if (!reqId) {
+      return;
+    }
+
+    this.logStorage[reqId] = {
+      ...this.logStorage[reqId],
+      [logObject.level]: [
+        ...(this.logStorage[reqId]?.[logObject.level] || []),
+        logObject,
+      ],
+    };
+  }
+
+  private bufferedLogsExistForRequest(reqId: string) {
+    return (
+      this.logStorage[reqId] !== undefined &&
+      typeof this.logStorage[reqId] === "object"
+    );
+  }
+
+  stringifyBufferedLogs(bufferedLogs: LogObject[]): string {
+    return bufferedLogs.map((log) => this.prettyPrinter(log)).join("");
+  }
 
   processLog(logObject: LogObject): string | null {
     const reqId = logObject.reqId || logObject.req?.id;
@@ -26,18 +107,33 @@ export class LogBuffer {
     }
 
     if (logObject.res) {
-      if (logObject.res?.statusCode >= 500) {
-        const bufferedLogs = this.logStorage[reqId];
-        return (
-          bufferedLogs?.map((log) => this.prettyPrinter(log)).join("") || null
-        );
+      const logsLevelsToRelease = this.shouldLogBufferedLogs(
+        logObject.res.statusCode
+      );
+
+      if (
+        logsLevelsToRelease.length &&
+        this.bufferedLogsExistForRequest(reqId)
+      ) {
+        let logPayload = "";
+
+        for (const logLevel of logsLevelsToRelease) {
+          const bufferedLogs =
+            this.logStorage[reqId]![logLevelToValueMap[logLevel]];
+
+          if (bufferedLogs) {
+            logPayload += this.stringifyBufferedLogs(bufferedLogs);
+          }
+        }
+
+        return logPayload || null;
       }
 
       delete this.logStorage[reqId];
       return null;
     }
 
-    this.logStorage[reqId] = [...(this.logStorage[reqId] || []), logObject];
+    this.addLogToBuffer(logObject);
 
     return null;
   }
--- transport.ts
+++ transport.ts
@@ -1,11 +1,10 @@
 import build from "pino-abstract-transport";
-import SonicBoom from "sonic-boom";
 import { pipeline, Transform } from "node:stream";
-import { LogBuffer } from "./log-buffer";
+import { LogBuffer, LogBufferOptions } from "./log-buffer";
 
-export const transportFn = async (opts: any) => {
-  const destination = getStream(opts.destination);
-  const logBuffer = new LogBuffer();
+export default async (opts: LogBufferOptions) => {
+  const destination = process.stdout;
+  const logBuffer = new LogBuffer(opts);
 
   return build((source) => {
     const stream = new Transform({
@@ -17,13 +16,6 @@ export const transportFn = async (opts: any) => {
       },
     });
 
-    // @ts-ignore
     pipeline(source, stream, destination, () => {});
   });
 };
-
-function getStream(fileDescriptor: any) {
-  if (fileDescriptor === 1 || !fileDescriptor) return process.stdout;
-  if (fileDescriptor === 2) return process.stderr;
-  return new SonicBoom({ dest: fileDescriptor, sync: false });
-}
